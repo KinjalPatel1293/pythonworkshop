0878c39ea37d12d45d5751d46eeb5fd31468d7f6
unknown <kinjal.patel1293@gmail.com>
2016-01-10 16:50:32 +0530

    new file uploaded

---
+++ big.py
@@ -0,0 +1,29 @@
+#odd even condition
+a=2 #var1
+b=4#var2
+c=3 #var3
+
+#ifelse statement
+'''if a>b:
+	if a>c:
+		print"a is big"
+	
+elif b>c:
+	print"b is big"
+
+else:
+	print"c is big"'''
+	
+
+#if else using AND operator
+'''if a>b and a>c:
+	print"a is big"
+
+elif b>a and b>c:
+	print "b is big"
+else:
+	print"c is big"'''
+
+#inbuilt function MAX
+
+print max(a,b,c)
